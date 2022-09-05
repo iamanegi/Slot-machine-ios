5192b010375608ccfb7b90b654bed681b52fb783
aman <amannegi229@gmail.com>
2022-09-05 12:32:05 +0530

    Initial Commit

---
+++ Shared/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  Shared
+//
+//  Created by Aman Negi on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
